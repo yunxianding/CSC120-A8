daeff89081efb00f94beafb755c79b8104f276a4
yunxiann <yding41@smith.edu>
2025-04-07 20:57:46 -0400

    Finished Library.java

--- Library.java
+++ Library.java
@@ -7,49 +7,92 @@ import java.util.Hashtable;
  */
 public class Library extends Building implements LibraryRequirements{
 
-  //New attributes
-  private Hashtable<String, Boolean> collection;
+    //New attributes
+    private Hashtable<String, Boolean> collection;
+    private boolean hasElevator;
 
-  /**
-   * Constructor for the Library class.
-   * @param name The name of the library.
-   * @param address The address of the library.
-   * @param nFloors The number of floors in the library.
-   */
-  public Library(String name, String address, int nFloors){
-    super(name, address, nFloors);
-    this.collection = new Hashtable<String, Boolean>();
-    System.out.println("You have built a library: 📖");
-  }
-
-  /**
-   * Adds a title to the library if it does not already exist.
-   * @param title The title to add.
-   */
-  public void addTitle(String title) {
-    if (!this.collection.containsKey(title)) {
-      this.collection.put(title, true);
-      System.out.println(title + " has been added to the library.");
-    } else {
-      System.out.println(title + " is already in the library.");
-      }
+    /**
+    * Constructor for the Library class.
+    * @param name The name of the library.
+    * @param address The address of the library.
+    * @param nFloors The number of floors in the library.
+    * @param hasElevator Whether the library has an elevator.
+    */
+    public Library(String name, String address, int nFloors, boolean hasElevator) {
+        super(name, address, nFloors);
+        this.collection = new Hashtable<String, Boolean>();
+        this.hasElevator = hasElevator;
+        System.out.println("You have built a library: 📖");
     }
 
-  /**
-   * Removes a title from the library if it exists.
-   * @param title The title to remove.
-   * @return The title that was removed, or null if it was not found.
-   */
-  public String removeTitle(String title) {
-    if (this.collection.containsKey(title)) {
-      this.collection.remove(title);
-      System.out.println(title + " has been removed from the library.");
-      return title;
-    } else {
-      System.out.println(title + " is not in the library.");
-      return null;
-      }
+    /* Overloaded constructor (defaults to no elevator) */
+    public Library(String name, String address, int nFloors) {
+        this(name, address, nFloors, false);
     }
+
+    /**
+    * Overrides the showOptions method to include Library-specific options.
+    */
+    @Override
+    public void showOptions() {
+        super.showOptions();
+        System.out.println(" + addTitle(String title) \n + removeTitle(String title) \n + checkOut(String title) \n + returnBook(String title) \n + printCollection()");
+    }
+
+    /**
+    * Overrides the goToFloor method to account for whether the library has an elevator.
+    * @param floorNum The floor to go to.
+    */
+    @Override
+        public void goToFloor(int floorNum) {
+        if (!this.hasElevator && (floorNum > this.activeFloor + 1 || floorNum < this.activeFloor - 1)) {
+        throw new RuntimeException("This library does not have an elevator. You can only move to adjacent floors.");
+        }
+        super.goToFloor(floorNum);
+    }
+
+    /**
+    * Adds a title to the library if it does not already exist.
+    * @param title The title to add.
+    */
+    public void addTitle(String title) {
+        if (!this.collection.containsKey(title)) {
+        this.collection.put(title, true);
+        System.out.println(title + " has been added to the library.");
+        } else {
+        System.out.println(title + " is already in the library.");
+        }
+    }
+
+    /* Overloaded addTitle method (adds multiple titles at once) */
+    public void addTitle(String[] titles) {
+        for (String title : titles) {
+          this.addTitle(title);
+        }
+    }
+
+    /**
+    * Removes a title from the library if it exists.
+    * @param title The title to remove.
+    * @return The title that was removed, or null if it was not found.
+    */
+    public String removeTitle(String title) {
+        if (this.collection.containsKey(title)) {
+        this.collection.remove(title);
+        System.out.println(title + " has been removed from the library.");
+        return title;
+        } else {
+        System.out.println(title + " is not in the library.");
+        return null;
+        }
+    }
+
+    /* Overloaded removeTitle method (removes multiple titles at once) */
+    public void removeTitle(String[] titles) {
+        for (String title : titles) {
+          this.removeTitle(title);
+        }
+      }
 
     /**
      * Checks if a title is available in the library.
@@ -101,12 +144,12 @@ public class Library extends Building implements LibraryRequirements{
     public void printCollection() {
         System.out.println("Library Collection:");
         for (String title : this.collection.keySet()) {
-          String status;
-          if (this.collection.get(title)) {
-              status = "Available";
-          } else {
-              status = "Checked out";
-          }
+            String status;
+            if (this.collection.get(title)) {
+                status = "Available";
+            } else {
+                status = "Checked out";
+            }
             System.out.println("- " + title + " (" + status + ")");
         }
     }
@@ -116,30 +159,39 @@ public class Library extends Building implements LibraryRequirements{
      * @param args Command-line arguments.
      */
     public static void main(String[] args) {
-      Library myLibrary = new Library("Neilson Library", "7 Neilson Drive", 4);
 
-      // Add books to the library
-      myLibrary.addTitle("The Old Man and the Sea by Ernest Hemingway");
-      myLibrary.addTitle("Animal Farm by George Orwell");
-      myLibrary.addTitle("The Design of Everyday Things by Don Norman");
+        System.out.println("------------------------------------");
+        System.out.println("Test of Building constructor/methods");
+        System.out.println("------------------------------------");
+        Library neilsonLibrary = new Library("Neilson Library", "7 Neilson Drive", 4, true);
+        
+        System.out.println(neilsonLibrary);
+        neilsonLibrary.showOptions();
 
-      // Print the collection
-      myLibrary.printCollection();
+        System.out.println("-----------------------------------");
+        System.out.println("Demonstrating enter/exit/navigation");
+        System.out.println("-----------------------------------");
+        neilsonLibrary.enter();
+        neilsonLibrary.goUp();
+        neilsonLibrary.goDown();
+        neilsonLibrary.goToFloor(4); //This should work because the library has an elevator
+        // neilsonLibrary.exit(); This should throw an exception because we can't exit from the 4th floor
+        neilsonLibrary.goToFloor(1);
+        neilsonLibrary.exit(); //This should work because we are on the first floor
 
-      // Check out a book
-      myLibrary.checkOut("The Design of Everyday Things by Don Norman");
-      myLibrary.checkOut("The Design of Everyday Things by Don Norman"); // This should fail because it's already checked out
+        // Add books to the library
+        neilsonLibrary.addTitle("The Old Man and the Sea by Ernest Hemingway");
+        String[] newBooks = {"Animal Farm by George Orwell", "The Design of Everyday Things by Don Norman"};
+        neilsonLibrary.addTitle(newBooks);
+        neilsonLibrary.printCollection();
 
-      // Return a book
-      myLibrary.returnBook("The Design of Everyday Things by Don Norman");
-      myLibrary.returnBook("Animal Farm by George Orwell"); // This should fail because it's not checked out
+        // Remove a book
+        neilsonLibrary.removeTitle("The Old Man and the Sea by Ernest Hemingway");
+        String[] removeBooks = {"Animal Farm by George Orwell", "The Design of Everyday Things by Don Norman"};
+        neilsonLibrary.removeTitle(removeBooks);
+        neilsonLibrary.printCollection();
 
-      // Remove a book
-      myLibrary.removeTitle("The Old Man and the Sea by Ernest Hemingway");
-
-      // Print the collection again
-      myLibrary.printCollection();
-  }
+    }
 }
   
   
--- reflection.md
+++ reflection.md
@@ -6,4 +6,10 @@ For **House.java**:
     2. I decided to 'overload' the moveIn method so that it accepts a student's name instead of a Student object. This doesn't make too much of a difference, just another way of achieving the same thing;
     3. I decided to 'overload' the moveOut method so that it accepts a student's name instead of a Student object. Same as 2;
     4. In order to make the two overload methods in 2 and 3 run without errors, I also overload the constructor of Student so that it can accepts only a name.
+For **Library.java**:
+    1. I decided to 'overload' the constructor so that without elevator information, it defaults to no elevator(most libraries have elevators but let's just prepare for the worst);
+    2. I decided to 'overload' the addTitle method so that we can add multiple titles as once. This is because it makes adding much easier if we are trying add a lot of books;
+    3. I decided to 'overload' the removeTitle method so that we can remove multiple titles as once. Same as 2.
+For **Cafe.java**:
+    1. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
